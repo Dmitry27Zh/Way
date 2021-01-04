4a3271754daf9391dca3d0c78d5e031021e56e83
Dmitry <dmitry27zh@gmail.com>
2021-01-04 22:10:12 +0300

    добавлен переход по табам

--- source/js/modal.js
+++ source/js/modal.js
@@ -2,6 +2,19 @@
 
 var buyButtonElements = document.querySelectorAll('.buy-js');
 var modalOrder = document.querySelector('.modal--order');
+var orderFormElemenent = modalOrder.querySelector('.order__form');
+var modalSuccess = document.querySelector('.modal--success');
+var feedbackFormElement = document.querySelector('.feedback__form');
+
+var onSuccessFeedback = function (evt) {
+  evt.preventDefault();
+  showModal(modalSuccess);
+};
+
+var feedbackSubmitHandler = function (evt) {
+  onSuccessFeedback(evt);
+  evt.target.removeEventListener('submit', feedbackSubmitHandler);
+};
 
 var showModal = function (modalElement) {
   modalElement.classList.add('modal--show');
@@ -10,9 +23,24 @@ var showModal = function (modalElement) {
   var closeModalHandler = function () {
     modalElement.classList.remove('modal--show');
     modalClose.removeEventListener('click', closeModalHandler);
+    document.removeEventListener('keydown', escKeydownHandler);
+  };
+  var escKeydownHandler = function (evt) {
+    if (evt.key === 'Escape') {
+      closeModalHandler();
+    }
   };
   modalClose.addEventListener('click', closeModalHandler);
   modalOverlay.addEventListener('click', closeModalHandler);
+  document.addEventListener('keydown', escKeydownHandler);
+  var orderSubmitHandler = function (evt) {
+    onSuccessFeedback(evt);
+    closeModalHandler();
+    evt.target.removeEventListener('submit', orderSubmitHandler);
+  };
+  if (modalElement === modalOrder) {
+    orderFormElemenent.addEventListener('submit', orderSubmitHandler);
+  }
 };
 
 buyButtonElements.forEach(function (button) {
@@ -20,4 +48,9 @@ buyButtonElements.forEach(function (button) {
     evt.preventDefault();
     showModal(modalOrder);
   });
+});
+
+feedbackFormElement.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  showModal(modalSuccess);
 });
